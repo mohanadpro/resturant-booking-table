d59a676646c352d87ad14aa9d60a52b9439d63d3
unknown <mohanad.13.93@gmail.com>
2024-12-07 18:46:50 +0100

    add links to social media

--- resturant_booking_table/urls.py
+++ resturant_booking_table/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meal/', include("meal.urls"), name="meal-urls")
+    path('meal/', include("meal.urls"), name="meal-urls"),
+    path('', include('reservation.urls'), name="reservation-urls")
 ]
