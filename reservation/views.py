5fc007b24970015080492cef45c7b72c3555bd0f
unknown <mohanad.13.93@gmail.com>
2024-12-10 23:19:10 +0100

    add meals to reservation page

--- reservation/views.py
+++ reservation/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from meal.models import Meal
 # Create your views here.
 def reservation(request):
-    
+    meal_list = Meal.objects.all()
+    paginate_by = 6
     return render(
         request,
         "reservation/reservation.html",
-        {'context':{}}
+        {'meal_list':meal_list}
     )
