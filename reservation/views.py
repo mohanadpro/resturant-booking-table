fa77159f1cbe41b0cfb7a5e708ca7f2288c5973a
unknown <mohanad.13.93@gmail.com>
2024-12-16 02:46:52 +0100

    add pagination to reservation list

--- reservation/views.py
+++ reservation/views.py
@@ -11,12 +11,6 @@ def reservation_list(request):
     temp_reservations = Reservation.objects.filter(customer=request.user).order_by("-created_on")
     p = Paginator(temp_reservations, 6)
     page_number =1 if request.GET.get('page') == None else request.GET.get('page')
-    # reservations = paginator.page(1)
-    # return render(
-    #     request,
-    #     'reservation/reservation_list.html',
-    #     {'reservations':reservations}
-    # )
     try:
         page_obj = p.get_page(page_number)  # returns the desired page object
     except PageNotAnInteger:
