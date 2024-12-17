272a9543f5edbe506a335d653a335b19f65959e4
unknown <mohanad.13.93@gmail.com>
2024-12-17 01:29:55 +0100

    delete all console.log commands

--- static/js/reservation.js
+++ static/js/reservation.js
@@ -8,20 +8,15 @@ const submitButton = document.getElementById("submitButton");
 const deleteButtons = document.getElementsByClassName("btn-delete");
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
-      console.log(e.target)
       let reservationId = e.target.getAttribute("reservation_id");
-      console.log(reservationId);
       deleteConfirm.href = `delete_reservation/${reservationId}`;
       deleteModal.show();
     });
   }
 
   for (let button of editButtons) {
-    console.log('HELLO')
     button.addEventListener("click", (e) => {
       let reservationId = e.target.getAttribute("reservation_id");
-
-      console.log(reservationId)
       // let reservation = document.getElementById(`reservation${reservationId}`).innerText;
       // commentText.value = commentContent;
       submitButton.innerText = "Update";
