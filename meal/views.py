809e67d357843e9646502bf5493d8e16a632c869
unknown <mohanad.13.93@gmail.com>
2024-12-17 01:28:19 +0100

    update meal app write it in python standard way

--- meal/views.py
+++ meal/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from .models import Meal
 # Create your views here.
 
+
 class MealList(generic.ListView):
     queryset = Meal.objects.all()
     template_name = 'meal/meal_list.html'
