281b6bdd94198679a3c8541ef8dc503247e52767
unknown <mohanad.13.93@gmail.com>
2024-12-17 02:43:08 +0100

    update font weight for nav bar in base.html

--- resturant_booking_table/settings.py
+++ resturant_booking_table/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-tcfc55+d_mvbcftqd4hv&&$&hv-w4#(pwse$z6h^!1q=^nf$ef
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 # '127.0.0.1:8000','.herokuapp.com'
 
 # Application definition
