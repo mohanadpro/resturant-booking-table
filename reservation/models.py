2c772e29c8dcdbcda61d5e447dcc47a58188da84
unknown <mohanad.13.93@gmail.com>
2024-12-17 01:20:11 +0100

    write reservation model in python standard way

--- reservation/models.py
+++ reservation/models.py
@@ -4,20 +4,23 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.safestring import mark_safe
 # Create your models here.
 
-AREAS = ((0, "Near to fireplace"), (1, "Terrace"), (2, "Smoker"), (3,"Inside"), (4, "Second floor"))
+AREAS = ((0, "Near to fireplace"),
+         (1, "Terrace"), (2, "Smoker"),
+         (3, "Inside"), (4, "Second floor"))
+
 
 class Reservation(models.Model):
     date = models.DateField()
     time = models.TimeField()
     area = models.IntegerField(choices=AREAS, default=0)
-    how_many_people = models.IntegerField(default=1,
+    how_many_people = models.IntegerField(
+        default=1,
         validators=[
             MaxValueValidator(25),
             MinValueValidator(1)
-        ] )
+        ])
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     have_kids = models.BooleanField(default=False)
-    special_request = models.CharField(max_length=250,null=True)
+    special_request = models.CharField(max_length=250, null=True)
     note = models.TextField(null=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    
