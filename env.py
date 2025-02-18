35ad45627045cac6bb878ced194d6fedea1f3d37
unknown <mohanad.13.93@gmail.com>
2025-02-18 15:24:03 +0100

    remove env.py file

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault("DEVELOPMENT","True")
-
-os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://732679134878388:1w7QZW1CMTNVIZDJgv_qmLkI-Vc@dqqbjpxys")
-
-os.environ.setdefault(
-    "SECRET_KEY", "i8-fq)1$o5_%+n+64q@ms8lvff9#(=(nbb*k7&nc)*!ws72a40")
