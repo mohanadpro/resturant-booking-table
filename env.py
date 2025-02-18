bd43bd2c2957844583ab89213de1403b73a7d73c
unknown <mohanad.13.93@gmail.com>
2025-02-18 12:06:52 +0100

    update secret key

--- env.py
+++ env.py
@@ -4,3 +4,6 @@ os.environ.setdefault("DEVELOPMENT","True")
 
 os.environ.setdefault(
     "CLOUDINARY_URL", "cloudinary://568255361399225:6p4gsV2o0iidnALWN_lmbHpQ3s0@dqqbjpxys")
+
+os.environ.setdefault(
+    "SECRET_KEY", "i8-fq)1$o5_%+n+64q@ms8lvff9#(=(nbb*k7&nc)*!ws72a40")
--- resturant_booking_table/settings.py
+++ resturant_booking_table/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tcfc55+d_mvbcftqd4hv&&$&hv-w4#(pwse$z6h^!1q=^nf$ef'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
